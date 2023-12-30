103c52ebb1b8a6d775e52f52ad207dbd56dc76ef
Daniel Wong <danielwongfarfan@gmail.com>
2023-12-29 21:00:55 -0600

    Video file is added in the Video model

--- video_transmission/api/serializers.py
+++ video_transmission/api/serializers.py
@@ -9,4 +9,6 @@ class VideoSerializer(AuditableSerializerMixin, serializers.ModelSerializer):
 
     class Meta:
         model = Video
-        fields = ['id', 'title', 'description', 'velocity']
+        fields = ['id', 'title', 'description', 'velocity', 'video_file']
+        # This is because video file was added after early version of video model
+        extra_kwargs = {"video_file": {"required": True, "allow_null": False}}
--- video_transmission/models.py
+++ video_transmission/models.py
@@ -4,10 +4,20 @@ from django.utils.translation import gettext as _
 from utils.models import AuditBaseModel
 
 
+def user_video_directory_path(instance, filename): 
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
+    return 'user_{0}/{1}'.format(instance.created_by.id, filename) 
+
+
 class Video(AuditBaseModel):
     title = models.CharField(max_length=100, verbose_name=_('Title'))
     description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
     velocity = models.IntegerField(verbose_name=_('Velocity'))
+    video_file = models.FileField(
+        verbose_name=_('Video File'), 
+        upload_to=user_video_directory_path,
+        null=True, blank=False
+    )
 
     def __str__(self) -> str:
         return self.title
